cee71dc0575f83f9bc6e7ef11fcd6623b17b0192
gholanda14 <gabriel.raulino14@gmail.com>
2022-01-15 21:34:45 -0300

    fix: foto ampliada fechando no x ou clicando fora

--- js/script.js
+++ js/script.js
@@ -2,23 +2,24 @@ let imagens = document.querySelectorAll('.foto');
 let modal = document.querySelector('.modals');
 let modalImg = document.querySelector("#modalImg");
 let btnClose = document.querySelector("#btnClose");
+let modalContents = document.querySelector(".modalContents");
 let srcValor = "";
 
 for(let i = 0; i < imagens.length;i++){
     imagens[i].addEventListener('click',function(){
         srcValor = imagens[i].getAttribute('src');
         modalImg.setAttribute('src',srcValor);
-        modal.classList.toggle('active');
+        modal.classList.add('active');
     });
 }
-
-
 
 /*Fechar modal*/
 btnClose.addEventListener('click',function(){
     modal.classList.remove('active');
 });
 
-modal.addEventListener('click',function(){
-    modal.classList.remove('active');
+window.addEventListener('click',function(event){
+    if(event.target == modalContents){
+        modal.classList.remove('active');
+    }
 });
