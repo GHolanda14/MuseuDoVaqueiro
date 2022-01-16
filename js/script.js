85c5dbbbcbe7f2f8d98c46f5d7e5a42c2ab62f04
gholanda14 <gabriel.raulino14@gmail.com>
2022-01-16 14:52:17 -0300

    style: dando responsividade ao botao de fechar

--- js/script.js
+++ js/script.js
@@ -10,8 +10,19 @@ for(let i = 0; i < imagens.length;i++){
         srcValor = imagens[i].getAttribute('src');
         modalImg.setAttribute('src',srcValor);
         modal.classList.add('active');
+        btnReposition();
     });
 }
+
+/*Posicionar o botão de fechar imagem com responsividade*/
+let btnReposition = function(){
+    btnClose.style.top = `${modalImg.offsetTop}px`;
+    btnClose.style.left = `${modalImg.offsetLeft + modalImg.offsetWidth - 40}px`;
+}
+
+window.addEventListener('resize',function(){
+    btnReposition();
+});
 
 /*Fechar modal*/
 btnClose.addEventListener('click',function(){
